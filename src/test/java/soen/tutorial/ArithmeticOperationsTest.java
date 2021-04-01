6d76d74c371497491d6d56968f70b51792f607ea
bowsong <43589884+bowsong@users.noreply.github.com>
2021-04-01 00:56:09 -0400

    Revert to pass

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -16,8 +16,8 @@ public class ArithmeticOperationsTest {
 		ArithmeticOperations operations = new ArithmeticOperations();
 		Integer actual = operations.addOrSub(2, 6);
 		Integer expected = 8;
-// 		assertEquals(expected, actual);
-		fail("Must fail");
+		assertEquals(expected, actual);
+// 		fail("Must fail");
 
 	}
 //     @Test
