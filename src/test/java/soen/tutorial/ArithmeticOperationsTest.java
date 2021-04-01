3d100bd162a52720614a4322f596b16612e2b649
bowsong <43589884+bowsong@users.noreply.github.com>
2021-04-01 00:57:09 -0400

    Revert to base

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -20,15 +20,15 @@ public class ArithmeticOperationsTest {
 // 		fail("Must fail");
 
 	}
-//     @Test
-//     public void testSub()
-//     {
+    @Test
+    public void testSub()
+    {
 
-//         ArithmeticOperations operations = new ArithmeticOperations();
-//         Integer actual = operations.addOrSub(6, 2);
-//         Integer expected = 4;
-//         assertEquals(expected, actual);
+        ArithmeticOperations operations = new ArithmeticOperations();
+        Integer actual = operations.addOrSub(6, 2);
+        Integer expected = 4;
+        assertEquals(expected, actual);
 
-//     }
+    }
 
 }
